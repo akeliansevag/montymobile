2a679a970347781eb5dc294e960637bc8840e93d
akeliansevag <akelian.sevag@gmail.com>
2023-08-29 18:40:01 +0300

    removed subscribe button from the products page

--- template-parts/product/actions.php
+++ template-parts/product/actions.php
@@ -1,8 +1,7 @@
 <section id="actions" class="product-actions py-5 bg-white">
     <div class="container">
         <div class="text-center">
-            <a href="" class="mm-button large-button mx-3">Contact us</a>
-            <a href="" class="mm-button large-button mx-3">Subscribe</a>
+            <a href="<?= home_url('/contact-us') ?>" class="mm-button large-button mx-3">Contact us</a>
         </div>
     </div>
 </section>
