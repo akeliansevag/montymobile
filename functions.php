1e6b79cee57d4c31b54b4a4abe7dce18305ab5be
akeliansevag <akelian.sevag@gmail.com>
2023-09-01 10:37:57 +0300

    .

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '8.2.8');
+	define('_S_VERSION', '8.2.9');
 }
 
 require_once 'custom-menu-walker.php';
