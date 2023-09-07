11b03d039e876b10da867805e176278c988b80be
akeliansevag <akelian.sevag@gmail.com>
2023-09-07 14:50:48 +0300

    .

--- functions.php
+++ functions.php
@@ -146,6 +146,15 @@ function build_menu_hierarchy($items, $parent_id = 0)
 	return $menu;
 }
 
-
+// Disable /users rest routes
+add_filter('rest_endpoints', function ($endpoints) {
+	if (isset($endpoints['/wp/v2/users'])) {
+		unset($endpoints['/wp/v2/users']);
+	}
+	if (isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])) {
+		unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+	}
+	return $endpoints;
+});
 
 require_once "blocks/register.php";
