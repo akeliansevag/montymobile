e885288ce63d674494a9b18af9b4b89c92486fce
akeliansevag <akelian.sevag@gmail.com>
2023-09-21 13:45:12 +0300

    .

--- blocks/product/related/related.php
+++ blocks/product/related/related.php
@@ -10,11 +10,11 @@ $blockID = $block['id'];
         <div class="container">
             <h2 class="fw-semi-bold"><?= $title ?></h2>
         </div>
-        <div class="related-products-container position-relative bg-gray py-5 mt-3 d-flex align-items-center px-3 px-md-0">
+        <div class="related-products-container position-relative bg-gray py-5 mt-3 d-flex align-items-center ">
             <img src="<?= get_template_directory_uri() ?>/assets/img/relatedbg2.jpg" alt="" class="position-absolute object-fit-cover top-0 left-0 z-0 w-100 h-100">
             <div class="container">
                 <div class="swiper-cont position-relative my-4">
-                    <div class="related-products-slider" id="related-products-slider-<?= $blockID ?>">
+                    <div class="related-products-slider px-3 px-md-0" id="related-products-slider-<?= $blockID ?>">
                         <?php foreach ($products as $p) : ?>
                             <div class="related-slide">
                                 <a href="<?= get_permalink($p->ID); ?>" class="product-slider-item mx-3 position-relative">
