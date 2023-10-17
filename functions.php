144dbf741762192f3acd2d71791e59119d1ecb4f
akeliansevag <akelian.sevag@gmail.com>
2023-10-17 13:14:00 +0300

    Chatbot script file

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '9.3.4');
+	define('_S_VERSION', '9.3.5');
 }
 
 require_once 'custom-menu-walker.php';
