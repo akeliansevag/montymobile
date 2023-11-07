7aa8fb665f8b1ef4b8f7403de4e0315b6dbb1803
akeliansevag <akelian.sevag@gmail.com>
2023-11-07 11:22:41 +0200

    was19 landing page

---
+++ page-was19.php
@@ -0,0 +1,150 @@
+<?php get_header(); ?>
+<section>
+    <div class="h-slider-item slide-1">
+        <div class="h-slider-background">
+            <img src="https://montymobile.com/wp-content/uploads/2023/10/waslanding.jpg" alt="">
+        </div>
+
+        <div class="h-slider-content">
+            <div class="text-center">
+                <div class="text-center">
+                    <img class="d-inline-block mb-2" src="https://montymobile.com/wp-content/uploads/2023/10/mmlogo.png" alt="Logo">
+                    <h2 style="letter-spacing:2px;" class="text-white mt-1 mb-3 fw-bold fs-2">HOSTING</h2>
+                    <img style="width:130px;" class="d-inline-block mb-2" src="https://montymobile.com/wp-content/uploads/2023/10/GSMALOGO.png" alt="GSMA Logo">
+                </div>
+
+
+                <h1 class="text-white">WAS#19</h1>
+                <div class="p-3 text-center mt-3 d-inline-block" style="background-color: var(--mmPink);">
+                    <p class="text-center p-0 m-0 text-white fw-bold">
+                        APRIL 16-18, 2024, <br>
+                        ISTANBUL, TURKEY
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-white py-5">
+    <div class="container">
+        <div class="row gx-0 gx-lg-5">
+            <div class="col-lg-6">
+                <h2>
+                    Istanbul, Turkey
+                </h2>
+                <p>
+                    A city where History Meets Modernity and ancient wonders coexist with contemporary marvels. With its rich history and vibrant ambiance, Istanbul is the perfect blend of tradition and modern allure.
+                </p>
+            </div>
+            <div class="col-lg-6">
+                <div class="d-flex">
+                    <div class="me-2">
+                        <img class="d-block img-fluid" src="https://montymobile.com/wp-content/uploads/2023/11/WAS-landing-page-02.webp" alt="WAS Image">
+                    </div>
+                    <div class="ms-2">
+                        <img class="d-block img-fluid" src="https://montymobile.com/wp-content/uploads/2023/11/WAS-landing-page-03.webp" alt="WAS Image">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-gray py-5">
+    <div class="container">
+        <h2 class="mb-4">Istanbul Congress Center ICC</h2>
+        <div class="row g-0 gx-lg-5 align-items-center">
+            <div class="col-lg-6">
+                <div class="d-flex mb-3 mb-lg-0">
+                    <div class="me-2">
+                        <img class="d-block img-fluid" src="https://montymobile.com/wp-content/uploads/2023/11/WAS-landing-page-04.webp" alt="WAS Image">
+                    </div>
+                    <div class="ms-2">
+                        <img class="d-block img-fluid" src="https://montymobile.com/wp-content/uploads/2023/11/WAS-landing-page-05.webp" alt="WAS Image">
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="col-lg-6">
+                <p>
+                    Where Innovation Meets Elegance! An unparalleled venue that defines sophistication and international business. As the city's largest congress center, ICC stands as a hub for global gatherings, offering a premier space for conferences, meetings, and events.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="h1 mb-5">Wholesale Agreements
+                    and Solutions Group</h2>
+            </div>
+        </div>
+        <div class="row gx-0 gx-lg-5 gy-3">
+            <div class="col-lg-6">
+                <h2>Overview</h2>
+                <p>
+                    The Wholesale Agreements and Solutions Group (WAS) is responsible for Operator-to-Operator and Operator-to-Hub interoperability (including Interworking and Roaming) to provide GSMA members with a consistent, reliable GSM service for members to offer their customers.
+                </p>
+            </div>
+            <div class="col-lg-6">
+                <h2>
+                    Key Topics
+                </h2>
+                <p>
+                    The Wholesale Agreements and Solutions Group (WAS) develops roaming and interconnect agreement templates and wholesale charging principles. Additionally, WAS supports GSMA projects with the development of roaming and interconnect agreement templates and wholesale charging principles that are unique to the needs of these efforts, as appropriate.
+                </p>
+            </div>
+        </div>
+
+        <div class="border border-white rounded-5 p-4 mt-4">
+            <h2>
+                What the Wholesale Agreements
+                and Solutions Group Does
+            </h2>
+            <p>
+                Develop and maintain roaming and interconnect agreement templates, solutions and charging frameworks as listed below:
+            <ul>
+                <li>Support of new prioritised projects and study of impacts;</li>
+                <li>Dialogue with GSMA leadership and provide recommendations on all matters related to roaming and interconnect;</li>
+                <li>Continuous improvements of official documentation</li>
+            </ul>
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="bg-white py-5">
+    <div class="container">
+        <h2>Sponsorships</h2>
+        <p class="mb-0">For sponsorship inquiries, please reach out on <a class="text-decoration-underline" href="mailto:was19_sponsorships@montymobile.com ">was19_sponsorships@montymobile.com </a></p>
+    </div>
+
+</section>
+<section class="bg-gray py-5">
+    <div class="container">
+        <h2>Hotels</h2>
+        <p class="mb-0">Coming Soon!</p>
+    </div>
+</section>
+<section class="bg-white py-5">
+    <div class="container">
+        <h2>Travel Details</h2>
+        <p class="mb-0">Coming Soon!</p>
+    </div>
+</section>
+<section class="bg-gray py-5">
+    <div class="container">
+        <h2>Agenda</h2>
+        <p class="mb-0">Coming Soon!</p>
+    </div>
+</section>
+<section class="bg-white py-5">
+    <div class="container">
+        <h4 class="m-0 text-center">For general inquiries, please contact <a class="text-decoration-underline" href="mailto:was19@montymobile.com">was19@montymobile.com </a> </h4>
+    </div>
+</section>
+<?php get_footer(); ?>
