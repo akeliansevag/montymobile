f7e75e283237500406df7d016f8560301232a31c
montyholdingdev <sevag.akelian@montyholding.com>
2024-02-08 13:01:34 +0200

    ,.

---
+++ template-parts/was19-party.php
@@ -0,0 +1,31 @@
+<section class="bg-white py-5">
+    <div class="container">
+        <h2 class="fs-1">WAS#19 Party by Monty Mobile</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <p>
+                    <strong>Date:</strong> April 17th<br>
+                    <strong>Time:</strong> Starting at 7:00 PM<br>
+                    <strong>Location:</strong> Basilica Cistern<br>
+                    <strong>Dress Code:</strong> Business Casual (comfortable footwear is recommended due to a 5 to 10-minute walking distance)<br>
+                    <strong>Transportation:</strong> Bus shuttles will be available from ICC starting at 7:00 PM, ensuring a convenient journey to and from the venue until 01:00.
+                </p>
+
+                <p>We invite all WAS#19 participants to step into an enchanting evening where history meets modern glam at the Basilica Cistern! As the sun sets over Istanbul, join us for a journey into the depths of this ancient marvel.</p>
+
+                <p> Our promise?<br>
+                    A night filled with awesome food, refreshing drinks, and entertainment that will have you dancing till dawn. It's a celebration, a soirée, and an all-out good time!
+
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p>
+                    <strong>Basilica Cistern</strong></br>
+                    Nestled beneath Istanbul's streets, the Basilica Cistern is a historic marvel built in the 6th century. Originally a reservoir for the Byzantine-era Great Palace, it features vast chambers supported by numerous marble columns, showcasing impressive ancient architecture.
+                </p>
+                <img class="w-100 mt-3" src="https://montymobile.com/wp-content/uploads/2024/02/was-Party.webp" alt="">
+            </div>
+        </div>
+
+    </div>
+</section>
