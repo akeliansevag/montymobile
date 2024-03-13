f5bc1a5357c8880812c7a5a8b69a78e65ba68a9d
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-03-13 10:22:41 +0200

    was19 montyesim

--- page-was19.php
+++ page-was19.php
@@ -323,6 +323,7 @@
 
 <?= get_template_part("template-parts/was19-party"); ?>
 
+<?= get_template_part("template-parts/monty-esim"); ?>
 <section class="bg-white py-5">
     <div class="container">
         <h4 class="m-0 text-center">For general inquiries, please contact <a class="text-decoration-underline" href="mailto:was19@montymobile.com">was19@montymobile.com </a> </h4>
---
+++ template-parts/monty-esim.php
@@ -0,0 +1,27 @@
+<section class="bg-white py-5">
+    <div class="container">
+        <h2 class="fs-1">Monty eSIM</h2>
+        <div class="mt-4">
+            <div>
+                <p class="mb-0"><strong>No.</strong> You do not have to wait in queues to get your SIM Card.</p>
+                <p><strong>No.</strong> You don't have to deal with expensive roaming fees.</p>
+            </div>
+
+
+            <p>Download <span style="color:var(--mmPink)"><strong>Monty eSIM App</strong></span> & Get <span style="color:var(--mmPink)"><strong>instant affordable 4G+</strong></span> connection.</p>
+
+            <div class=" row">
+                <div class="col-lg-4">
+                    <img width="200px" class="mt-3" src="https://montymobile.com/wp-content/uploads/2024/03/montyesimqr-e1710317688259.png" alt="qr-code" />
+                    <p class="mt-3">Available on iOS & Android</p>
+
+                    <p><strong>*Turkish data bundles start at just $2.</strong></p>
+                </div>
+                <div class="col-lg-8">
+                    <img width="100%" src="https://montymobile.com/wp-content/uploads/2024/03/montyesimpic-e1710317775968.jpg" alt="plane-pic" />
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
