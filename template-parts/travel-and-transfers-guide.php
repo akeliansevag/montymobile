5025841fff4080bf5e0a9a24819e8df96bfa1c34
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-03-15 09:23:50 +0200

    changed travel and transfer guide pdf

--- template-parts/travel-and-transfers-guide.php
+++ template-parts/travel-and-transfers-guide.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h2 class="fs-1">Travel and Transfers Guide</h2>
         <div class="mt-4">
-            <p><a href="https://montymobile.com/wp-content/uploads/2024/03/WAS19-Website-Travel-Transfers-Guide2.pdf" class="mm-button" target="_blank">Click here to download</a> </p>
+            <p><a href="https://montymobile.com/wp-content/uploads/2024/03/WAS19-Website-Travel-Transfers-Guide-.pdf" class="mm-button" target="_blank">Click here to download</a> </p>
         </div>
     </div>
 </section>
