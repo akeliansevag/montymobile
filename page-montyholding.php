b5bf0aaa7fa563606e4bd119d3d8d9522eb2d0b2
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-08-27 15:53:24 +0300

    .

--- page-montyholding.php
+++ page-montyholding.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
+<style>
+    #wrapper {
+        margin-top: 0;
+    }
+</style>
 <img style="width: 100%;" src="https://montymobile.com/wp-content/uploads/2024/08/Homepage-Option22.jpg" alt="">
 <?php get_footer(); ?>
