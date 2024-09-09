64a6023e3bb0072527ab3aa0b6fc2dcf94f016cc
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-09-09 14:48:00 +0300

    .

--- single-product-esim-entitlement-server.php
+++ single-product-esim-entitlement-server.php
@@ -17,21 +17,44 @@
             </div>
         </div>
 
-        <div class="boxed text-start">
-            <h2>One Number Service</h2>
-            <p>uPair can fully lifecycle manage the One Number product behind the Apple Watch, Samsung Gear, and Wear OS enabling MNOs subscribers to have two more voice/video/messaging devices under one mobile service number. Once the secondary service is paired the customer will be able to receive voice/video calls as well as send and receive messages from the secondary device. </p>
+        <div class="boxed text-start with-icon position-relative">
+            <img class="image-icon" src="<?= get_template_directory_uri() ?>/assets/img/esim/icon-1.webp" alt="Icon 1">
+            <div>
+                <h2>One Number Service</h2>
+                <p>uPair can fully lifecycle manage the One Number product behind the Apple Watch, Samsung Gear, and Wear OS enabling MNOs subscribers to have two more voice/video/messaging devices under one mobile service number. Once the secondary service is paired the customer will be able to receive voice/video calls as well as send and receive messages from the secondary device. </p>
+            </div>
+
         </div>
 
-        <div class="boxed text-start">
+        <div class="boxed text-start with-icon position-relative">
+            <img class="image-icon" src="<?= get_template_directory_uri() ?>/assets/img/esim/icon-1.webp" alt="Icon 1">
+            <div>
+                <h2>One Number Service</h2>
+                <p>uPair can fully lifecycle manage the One Number product behind the Apple Watch, Samsung Gear, and Wear OS enabling MNOs subscribers to have two more voice/video/messaging devices under one mobile service number. Once the secondary service is paired the customer will be able to receive voice/video calls as well as send and receive messages from the secondary device. </p>
+            </div>
+
+        </div>
+
+        <!-- <div class="boxed text-start">
+            <img src="<?= get_template_directory_uri() ?>/assets/img/esim/icon-2.webp" alt="Icon 2">
             <h2>eSIM Transfer</h2>
             <p>uPair can provide MNOs eSIM support including eSIM wearables and eSIM transfer through it’s Device Entitlement Server (DES)..</p>
         </div>
 
         <div class="boxed text-start">
+            <img src="<?= get_template_directory_uri() ?>/assets/img/esim/icon-3.webp" alt="Icon 3">
             <h2>VoWIFI / Volte Entitlement</h2>
             <p>Volte and VoWiFi Activatio, The VoLTE is an optional flow that may be used for operators wishing to use Entitlements to manage the deployment of VoLTE to capable Apple iPhones.</p>
-        </div>
+        </div> -->
     </div>
 </section>
+<style>
+    .image-icon {
+        position: absolute;
+        top: 0;
+        height: 200%;
+        transform: translate(-50%, -25%);
+    }
+</style>
 <?php get_template_part("template-parts/contact-us"); ?>
 <?php get_footer(); ?>
